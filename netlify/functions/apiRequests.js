c00668f287dd355e892185058e48842c205e7f18
Patrick Tobias <hej@patrickdanielsson.se>
2024-11-09 19:01:04 +0100

    Update apiRequests.js

--- netlify/functions/apiRequests.js
+++ netlify/functions/apiRequests.js
@@ -3,36 +3,53 @@ exports.handler = async (event) => {
         // Parse request body
         const { action, userName, userId, discordId } = JSON.parse(event.body);
 
-        // Select the appropriate API endpoint and body
+        // Initialize variables
         let url = '';
-        let body = {};
+        let options = {};
         let successMessage = '';
         let errorMessages = {};
 
+        // Handle different actions
         if (action === 'firstButton') {
-            url = 'https://primepnp.retool.com/url/telegram/selfassign';
-            body = { user: userName };
-            successMessage = 'Thank you for helping out!';
+            // Updated API endpoint for 'firstButton' action (GET request)
+            url = `https://script.google.com/macros/s/AKfycbyU-LmqCxdMcXD_di6hxtm__9KgdZLHIjN49804jPLDfm8e4JOOFk8CyRYwUgIJyKJRiQ/exec?telegramUser=${userName}`;
+            successMessage = 'You will be assigned co-host soon, try again in 1 min if you still haven’t been assigned co-host.';
             errorMessages = {
                 500: 'Error: Please try again.',
-                400: 'This user is not linked to the hostbot.'
+                400: 'Your Telegram user has no link to any Discord user.'
+            };
+            
+            // Configure GET request options
+            options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
             };
         } else if (action === 'linkDiscord') {
+            // Keep the original API endpoint for linking Discord (POST request)
             url = 'https://primepnp.retool.com/url/telegram/link';
-            body = { telegramUser: userName, discordId: discordId, telegramId: userId };
+            const body = {
+                telegramUser: userName,
+                discordId: discordId,
+                telegramId: userId
+            };
             successMessage = 'Your accounts have been linked!';
+            
+            // Configure POST request options
+            options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body)
+            };
         }
 
-        // Send API request
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
+        // Send the API request
+        const response = await fetch(url, options);
 
-        // Handle response
+        // Handle the response
         if (response.ok) {
             return {
                 statusCode: 200,
